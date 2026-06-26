8c34eec951013c8844754f8d30c8d01cf9adfc5e
VolodymyrNakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2026-06-26 16:40:59 +0300

    refactor(user-web): extract projects loading skeleton

--- apps/user-web/src/views/ProjectView.vue
+++ apps/user-web/src/views/ProjectView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import AutoComplete from "primevue/autocomplete";
 import Select from "primevue/select";
-import Skeleton from "primevue/skeleton";
 import { computed } from "vue";
 import { useConfirm } from "primevue/useconfirm";
 import { useToast } from "primevue/usetoast";
@@ -12,6 +11,7 @@ import {
 } from "@gitiempo/web-shared";
 
 import ProjectTaskDialog from "@/components/projects/ProjectTaskDialog.vue";
+import ProjectsLoadingSkeleton from "@/components/projects/ProjectsLoadingSkeleton.vue";
 import ProjectsTaskSection from "@/components/projects/ProjectsTaskSection.vue";
 import { useProjectTaskActions } from "@/composables/projects/useProjectTaskActions";
 import { useProjectTaskBackfillFlow } from "@/composables/projects/useProjectTaskBackfillFlow";
@@ -138,77 +138,7 @@ async function retryLoadPage(): Promise<void> {
 
 <template>
   <section class="flex flex-col gap-6 pb-20 sm:pb-0">
-    <template v-if="pageState === 'loading'">
-      <div class="flex flex-col gap-3 sm:flex-row sm:flex-wrap">
-        <Skeleton
-          width="22.5rem"
-          height="1rem"
-        />
-        <Skeleton
-          width="11.25rem"
-          height="1rem"
-        />
-        <Skeleton
-          width="11.25rem"
-          height="1rem"
-        />
-      </div>
-
-      <div class="flex flex-col gap-3 sm:flex-row sm:flex-wrap">
-        <Skeleton
-          width="22.5rem"
-          height="2.75rem"
-        />
-        <Skeleton
-          width="11.25rem"
-          height="2.75rem"
-        />
-        <Skeleton
-          width="11.25rem"
-          height="2.75rem"
-        />
-      </div>
-
-      <div class="flex flex-col gap-5">
-        <div
-          v-for="index in 2"
-          :key="index"
-          class="flex flex-col gap-2.5"
-        >
-          <div class="flex items-center justify-between gap-3">
-            <div class="flex flex-col gap-2">
-              <Skeleton
-                width="10rem"
-                height="1.25rem"
-              />
-              <Skeleton
-                width="6rem"
-                height="0.875rem"
-              />
-            </div>
-            <Skeleton
-              width="6.5rem"
-              height="2rem"
-            />
-          </div>
-
-          <div class="border-divider bg-surface-primary flex flex-col rounded-lg border">
-            <Skeleton
-              width="100%"
-              height="2.75rem"
-            />
-            <Skeleton
-              width="100%"
-              height="3.25rem"
-            />
-            <Skeleton
-              width="100%"
-              height="3.25rem"
-            />
-          </div>
-        </div>
-      </div>
-    </template>
+    <ProjectsLoadingSkeleton v-if="pageState === 'loading'" />
 
     <template v-else>
       <div
